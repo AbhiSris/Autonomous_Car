480da906a59b7d8d9cd7cd3e150db61522a9e6c7
Abhiram Srikantharajah <a7srikantharajah@uwaterloo.ca>
2017-01-07 19:53:53 -0500

    updated ultrasonic functions and code. right now, the car stops whenever it reaches an object 50 meters away

--- autonomous_car/autonomous_car.ino
+++ autonomous_car/autonomous_car.ino
@@ -3,11 +3,13 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 // Define constants for motors
-
 #define INIT_FRONT_SPEED 75
-#define INIT_BACK_SPEED 75
+#define INIT_BACK_SPEED 30
 #define INCREASE_FRONT_MOTOR 10
+#define US_UPPER_LIMIT 200
+#define US_LOWER_LIMIT 0
 
+// Motor definitions
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *frontMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *backMotor = AFMS.getMotor(3);
@@ -15,93 +17,97 @@ Adafruit_DCMotor *backMotor = AFMS.getMotor(3);
 int mOn = 8;
 int mOff = 9;
 
-int bs1 = 0;
-int bs2 = 0;
+int start_button = 0;
 
 //Sonar 1
-int echoPin1 =2;
-int initPin1 =3;
-int distance1 =0;
+int echoPinL =2;
+int trigPinL =3;
+int distanceL =0;
 
 //Sonar 2
-int echoPin2 =4;
-int initPin2 =5;
-int distance2 =0;
+int echoPinC =4;
+int trigPinC =5;
+int distanceC =0;
 
 //Sonar 3
-int echoPin3 =6;
-int initPin3 =7;
-int distance3 =0;
+int echoPinR =6;
+int trigPinR =7;
+int distanceR =0;
+
+int distVals[3] = {0,0,0};
 
 void setup() {
-
-  pinMode(initPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(initPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
-  pinMode(initPin3, OUTPUT);
-  pinMode(echoPin3, INPUT);
   Serial.begin(9600);
   AFMS.begin();  // create with the default frequency 1.6KHz
+
+  pinMode(trigPinL, OUTPUT);
+  pinMode(echoPinL, INPUT);
+  pinMode(trigPinC, OUTPUT);
+  pinMode(echoPinC, INPUT);
+  pinMode(trigPinR, OUTPUT);
+  pinMode(echoPinR, INPUT);
+  pinMode(mOn, INPUT);
+  pinMode(mOff, INPUT);
 
   frontMotor->setSpeed(INIT_FRONT_SPEED);
   backMotor->setSpeed(INIT_BACK_SPEED);
 
-  pinMode(mOn, INPUT);
-  pinMode(mOff, INPUT);
+  // Loop won't start without clicking the start button
+  start_button = digitalRead(mOn);
+  while (!start_button) {
+    start_button = digitalRead(mOn);
+  }
+  motor_forward(backMotor);
+  
 
 }
 
 void loop() {
-  bs1 = digitalRead(mOn);
-  bs2 = digitalRead(mOff);
-
-//  Serial.print("L: ");
-//  distance1 = getDistance(initPin1, echoPin1);
-//  printDistance(distance1);
-//  delay(150);
-//    
-//  Serial.print("C: ");  
-//  distance2 = getDistance(initPin2, echoPin2);
-//  printDistance(distance2);
-//  delay(150);
-//  
-//   Serial.print("R: ");  
-//  distance3 = getDistance(initPin3, echoPin3);
-//  printDistance(distance3);
-//  delay(150);
-//
-//  Serial.println(" ");
-
   
+    getAllDistances();
 
-  if (bs1){
-    motor_forward(backMotor);
-  }
-  else if (bs2){
-    motor_off(backMotor);
-  }
-
-  //delay(500);
-
+    if (distVals[1] < 50) {
+      motor_off(backMotor);
+    }
+    
+    start_button = digitalRead(mOn);
+    if (start_button) {
+      motor_forward(backMotor);
+    }
 }
 
-int getDistance (int initPin, int echoPin){
-
-  digitalWrite(initPin, HIGH);
+/* 
+ *  ULTRASONIC CONTROL CODE
+ */
+int getDistance (int trigPin, int echoPin){
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(10); 
-  digitalWrite(initPin, LOW); 
+  digitalWrite(trigPin, LOW); 
   unsigned long pulseTime = pulseIn(echoPin, HIGH); 
   int distance = (pulseTime/2) / 29.1;
   return distance;
+}
 
+void getAllDistances () {
+  distVals[0] = getDistance(trigPinL, echoPinL);
+  distVals[1] = getDistance(trigPinC, echoPinC);
+  distVals[2] = getDistance(trigPinR, echoPinR);
+
+  Serial.print("L: ");
+  printDistance(distVals[0]);
+  Serial.print("C: ");
+  printDistance(distVals[1]);
+  Serial.print("R: ");
+  printDistance(distVals[2]);
+  Serial.println(" ");
+  delay(150);
 }
 
 void printDistance(int dist){
-  if (dist >= 200 || dist <= 0 ){
+  if (dist >= US_UPPER_LIMIT || dist <= US_LOWER_LIMIT ) {
     Serial.println(" Out of range");
   }
-  else{
+  else {
     Serial.print(dist);
     Serial.println(" cm");
   }
@@ -110,7 +116,6 @@ void printDistance(int dist){
 /*
  *  MOTOR CONTROL CODE 
  */
-
 void motor_forward(Adafruit_DCMotor *inputMotor){
   inputMotor->run(FORWARD);
   delay(10);
