ecb5868b6e45e4fb4e241b77d7228def33b2a847
Abhiram Srikantharajah <a7srikantharajah@uwaterloo.ca>
2017-03-15 22:11:20 -0400

    gps and servo were not working before because they were using the same time. added new library to switch servo to user timer2 instead of timer1 and they work.

--- autonomous_car/autonomous_car.ino
+++ autonomous_car/autonomous_car.ino
@@ -1,10 +1,13 @@
 #include <PID_v1.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
-#include <Servo.h>
 #include <math.h>
 #include <Mag.h>
+#include <ServoTimer2.h>  // the servo library
+#include <Adafruit_GPS.h> //Load the GPS Library. Make sure you have installed the library form the adafruit site above
+#include <SoftwareSerial.h> //Load the Software Serial Library. This library in effect gives the arduino additional serial ports
+
+#define rollPin  9
 
 // Define constants for motors
 #define INIT_FRONT_SPEED 240
@@ -13,16 +16,19 @@
 #define INCREASE_FRONT_MOTOR 10
 #define US_UPPER_LIMIT 200
 #define US_LOWER_LIMIT 0
-#define SERVO_INIT 70
 #define OBSTACLE_DIST 25
 #define TURNING_ANGLE 45
 #define ACCEL_TUNING 5
+#define SERVO_MAX 1760
+#define SERVO_INIT 1260
+#define SERVO_MIN 760
+#define GPS_CHECK 20
 
 // Motor definitions
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *frontMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *backMotor = AFMS.getMotor(3);
-Servo myservo;
+ServoTimer2 servoRoll;
 
 /*
    CODE FOR AUTONOMOUS
@@ -45,13 +51,13 @@ int stopButton = 6;
 int buttonState = 0;
 
 //Sonar 1
-int echoPinL = 2;
-int trigPinL = 3;
+int echoPinL = 4;
+int trigPinL = 5;
 int distanceL = 0;
 
 //Sonar 2
-int echoPinR = 4;
-int trigPinR = 5;
+int echoPinR = 6;
+int trigPinR = 7;
 int distanceC = 0;
 
 // Mag sensor value
@@ -64,10 +70,31 @@ int distVals[2] = {0, 0};
 Magnometer mag;
 
 int state = 0;
+int GPS_counter = 0;
 
+/*
+ * GPS Declarations
+ */
+
+SoftwareSerial mySerial(3, 2); //Initialize SoftwareSerial, and tell it you will be connecting through pins 2 and 3
+Adafruit_GPS GPS(&mySerial); //Create GPS object
+ 
+String NMEA1;  //We will use this variable to hold our first NMEA sentence
+String NMEA2;  //We will use this variable to hold our second NMEA sentence
+char c;       //Used to read the characters spewing from the GPS module
+int desiredLat = 0;
+int desiredLon = 0;
 
 void setup() {
+  servoRoll.attach(rollPin);
+  
   Serial.begin(9600);
+  GPS.begin(9600);       //Turn GPS on at baud rate of 9600
+  GPS.sendCommand("$PGCMD,33,0*6D"); // Turn Off GPS Antenna Update
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); //Tell GPS we want only $GPRMC and $GPGGA NMEA sentences
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  delay(1000);  //Pause
+  
   AFMS.begin();  // create with the default frequency 1.6KHz
 
   pinMode(trigPinL, OUTPUT);
@@ -78,15 +105,14 @@ void setup() {
   frontMotor->setSpeed(INIT_FRONT_SPEED);
   backMotor->setSpeed(INIT_BACK_SPEED);
 
-  myservo.attach(9);
-
   //  // Loop won't start without clicking the start button
   //  start_button = digitalRead(mOn);
   //  while (!start_button) {
   //    start_button = digitalRead(mOn);
   //  }
   //motor_forward(backMotor);
-  myservo.write(pos);
+
+  servoRoll.write(pos);
 
   mag.mag_setup();
 
@@ -105,20 +131,12 @@ void loop() {
      LOGIC FOR AUTONOMOUS
   */
 
-  buttonState = digitalRead(stopButton);
-
-
-
-  getAccelReadings();
-
+  state = 0;
+//  readGPS();
+  
+//  getAccelReadings();
   Serial.println(state);
   getAllDistances();
-
-  if (stopButton == HIGH) {
-    frontMotor->run(RELEASE);
-    backMotor->run(RELEASE);
-  }
-
 
   // State 1: both sensors see something, turn right for now
   if (distVals[0] < OBSTACLE_DIST && distVals[1] < OBSTACLE_DIST && !leftFlag && !rightFlag && !turningFlag) {
@@ -130,6 +148,7 @@ void loop() {
     frontMotor->run(FORWARD);
     turningFlag = true;
     rightFlag = true;
+    leftFlag = false;
     obstacleFlag = true;
   }
 
@@ -143,6 +162,7 @@ void loop() {
     frontMotor->run(BACKWARD);
     turningFlag = true;
     rightFlag = true;
+    leftFlag = false;
     obstacleFlag = true;
   }
 
@@ -156,6 +176,7 @@ void loop() {
     frontMotor->run(BACKWARD);
     turningFlag = true;
     leftFlag = true;
+    rightFlag = false;
     obstacleFlag = true;
   }
 
@@ -192,65 +213,43 @@ void loop() {
   if (turningStepperFlag && !turningStepperOrigFlag) {
     state = 5;
     Serial.println(state);
-    if (startBearing > currBearing && turningStepperFlag) {
-      while (startBearing > currBearing) {
-        pos += 1;
-        myservo.write(pos);
+    if (leftFlag && turningStepperFlag) {
+        pos = SERVO_MAX;
+        servoRoll.write(pos);
         Serial.println(pos);
-        delay(15);
-        getAccelReadings();
-        Serial.println("");
-        currBearing = mag.getAngle();
-        Serial.print("start: ");
-        Serial.print(startBearing);
-        Serial.print("curr: ");
-        Serial.println(currBearing);
-      }
-      turningStepperFlag == false;
+        turningStepperFlag = false;
+        delay(500);
     }
-    else if (startBearing < currBearing && turningStepperFlag){
-      while (startBearing < currBearing) {
-        pos -= 1;
-        myservo.write(pos);
+    else if (rightFlag && turningStepperFlag){
+        pos = SERVO_MIN;
+        servoRoll.write(pos);
         Serial.println(pos);
-        delay(15);
-        getAccelReadings();
-        Serial.println("");
-        currBearing = mag.getAngle();
-        Serial.print("start: ");
-        Serial.print(startBearing);
-        Serial.print("curr: ");
-        Serial.println(currBearing);
-      }
-      turningStepperFlag == false;
+        turningStepperFlag = false;
+        delay(500);
     }
     backMotor->run(FORWARD);
   }
 
-  // State 7: both sensors see something, turn right for now
+  // State 6: both sensors see something, turn right for now
   if (distVals[0] > OBSTACLE_DIST && distVals[1] > OBSTACLE_DIST && leftFlag && !turningFlag && obstacleFlag) {
-    state = 7;
+    state = 6;
     Serial.println(state);
     backMotor->run(RELEASE);
     getAccelReadings();
     currBearing = mag.getAngle();
-//    frontMotor->run(FORWARD);
-//    turningFlag = true;
     leftFlag = false;
     rightFlag = true;
     turningStepperFlag = true;
     turningStepperOrigFlag = true;
   }
 
-  // State 8: both sensors see something, turn right for now
+  // State 7: both sensors see something, turn right for now
   if (distVals[0] > OBSTACLE_DIST && distVals[1] > OBSTACLE_DIST && rightFlag && !turningFlag && obstacleFlag) {
     state = 8;
     Serial.println(state);
     backMotor->run(RELEASE);
     getAccelReadings();
     currBearing = mag.getAngle();
-//    frontMotor->run(BACKWARD);
-//    turningFlag = true;
     leftFlag = true;
     rightFlag = false;
     turningStepperFlag = true;
@@ -260,60 +259,21 @@ void loop() {
   if (turningStepperFlag && turningStepperOrigFlag) {
     state = 10;
     Serial.println(state);
-//    if (pos < SERVO_INIT && turningStepperOrigFlag) {
-//      while (pos < SERVO_INIT) {
-//        
-//        pos += 1;
-//        myservo.write(pos);
-//        Serial.println(pos);
-//        delay(15);
-//        getAccelReadings();
-//        Serial.println("");
-//        currBearing = mag.getAngle();
-//        Serial.print("start: ");
-//        Serial.print(startBearing);
-//        Serial.print("curr: ");
-//        Serial.println(currBearing);
-//      }
-//      turningStepperFlag == false;
-//      turningStepperOrigFlag == false;
-//    }
-//    else if (pos > SERVO_INIT && turningStepperOrigFlag){
-//      while (pos > SERVO_INIT) {
-//        pos -= 1;
-//        myservo.write(pos);
-//        Serial.println(pos);
-//        delay(15);
-//        getAccelReadings();
-//        Serial.println("");
-//        currBearing = mag.getAngle();
-//        Serial.print("start: ");
-//        Serial.print(startBearing);
-//        Serial.print("curr: ");
-//        Serial.println(currBearing);
-//      }
-
-      if ((abs(pos - SERVO_INIT) > 20) && turningStepperOrigFlag){
-        pos = SERVO_INIT;
-        myservo.write(pos);
-        delay(500);
-      }
-        
-      turningStepperFlag == false;
-      turningStepperOrigFlag == false;
-      turningFlag = true;
-      obstacleFlag = false;
+    if ( pos != SERVO_INIT /*(abs(pos - SERVO_INIT) > 20) */&& turningStepperOrigFlag){
+      pos = SERVO_INIT;
+      servoRoll.write(pos);
+      delay(500);
+    }
+    turningStepperFlag == false;
+    turningStepperOrigFlag == false;
+    turningFlag = true;
+    obstacleFlag = false;
   }
 
-  // State 9: while turning, stop at 45 deg
+  // State 8: while turning, stop at 45 deg
   if (turningFlag && !obstacleFlag) {
     state = 9;
     Serial.println(state);
-//    while (tuning_num < ACCEL_TUNING) {
-//      getAccelReadings();
-//      startBearing = mag.getAngle();
-//      tuning_num = tuning_num + 1;
-//    }
     tuning_num = 0;
     if (leftFlag) {
       frontMotor->run(FORWARD);
@@ -350,25 +310,28 @@ void loop() {
       }
       turningFlag = false;
     }
-    
-//    while ( abs(startBearing - currBearing) > 1) {
-//      getAccelReadings();
-//      Serial.println("");
-//      currBearing = mag.getAngle();
-//      Serial.print("start: ");
-//      Serial.print(startBearing);
-//      Serial.print("curr: ");
-//      Serial.println(currBearing);
-//    }
     backMotor->run(RELEASE);
     frontMotor->run(RELEASE);
     backMotor->setSpeed(INIT_BACK_SPEED);
     leftFlag = false;
     rightFlag = false;
+
+    readGPS();
   }
 
-  
 
+  // FIX THIS
+  if (state == 0) {
+    ++GPS_counter;
+  }
+  else {
+    GPS_counter = 0;
+  }
+
+  if (GPS_counter == GPS_CHECK) {
+    readGPS();
+    GPS_counter = 0;
+  }
 }
 
 /*
@@ -447,14 +410,71 @@ void getAccelReadings() {
   
   mag.incrementCount();
 
-
-
   Serial.print("Prev Angle is ");
   Serial.print(mag.getPrevAngle());
   Serial.print("  Curr Angle is ");
   Serial.println(mag.getAngle());
-  
 }
 
+/*
+ * GPS CODE
+ */
+
+ void readGPS() {  //This function will read and remember two NMEA sentences from GPS
+  clearGPS();    //Serial port probably has old or corrupt data, so begin by clearing it all out
+  while(!GPS.newNMEAreceived()) { //Keep reading characters in this loop until a good NMEA sentence is received
+    c=GPS.read(); //read a character from the GPS
+  }
+  GPS.parse(GPS.lastNMEA());  //Once you get a good NMEA, parse it
+  Serial.println(GPS.satellites);
+  NMEA1=GPS.lastNMEA();      //Once parsed, save NMEA sentence into NMEA1
+
+  Serial.print("\nTime: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", "); 
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Location (in degrees, works with Google Maps): ");
+      Serial.print(GPS.latitudeDegrees, 4);
+      Serial.print(", "); 
+      Serial.println(GPS.longitudeDegrees, 4);
+      
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    }
+  
+  while(!GPS.newNMEAreceived()) {  //Go out and get the second NMEA sentence, should be different type than the first one read above.
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA());
+  NMEA2=GPS.lastNMEA();
+  Serial.println(NMEA1);
+  Serial.println(NMEA2);
+  Serial.println("");
+}
+
+void clearGPS() {  //Since between GPS reads, we still have data streaming in, we need to clear the old data by reading a few sentences, and discarding these
+  while(!GPS.newNMEAreceived()) {
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA());
+  while(!GPS.newNMEAreceived()) {
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA());
+}
 
 
